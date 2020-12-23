6aec5a5e78dcd63eccfd104bbcaa1bf09f92b91f
Etienne92 <etienne.sollier@polytechnique.edu>
2020-12-23 13:35:38 +0100

    Remove last_layer_GNN

--- code/model.py
+++ code/model.py
@@ -29,30 +29,34 @@ class NN(nn.Module):
 
         if self.n_layers_GNN > 0:
             self.FC = False
-        print(self.FC)
 
+        # Fully connected layers. They occur after the graph convolutions (or at the start if there no are graph convolutions)
         if self.n_layers_FC > 0:
-            self.layers_FC.append(nn.Linear(n_features, n_hidden_FC[0]))
+            if self.n_layers_GNN==0:
+                self.layers_FC.append(nn.Linear(n_features, n_hidden_FC[0]))
+            else:
+                self.layers_FC.append(nn.Linear(n_hidden_GNN[-1], n_hidden_FC[0]))
             if self.n_layers_FC > 1:
-                print((self.n_layers_FC-2))
                 for i in range(self.n_layers_FC-1):
-                    print(i)
                     self.layers_FC.append(nn.Linear(n_hidden_FC[i], n_hidden_FC[(i+1)]))
-            self.last_layer_FC = nn.Linear(n_hidden_FC[(self.n_layers_FC-1)], n_classes)
+
+        # Last layer
+        if self.n_layers_FC>0:
+            self.last_layer_FC = nn.Linear(n_hidden_FC[-1], n_classes)
+        elif self.n_layers_GNN>0:
+            self.last_layer_FC = nn.Linear(n_features*n_hidden_GNN[-1], n_classes)
         else:
             self.last_layer_FC = nn.Linear(n_features, n_classes)
 
     def forward(self,x,edge_index):
-        if self.FC == True:
+        if self.FC:
             # Resize from (1,batch_size * n_features) to (batch_size, n_features)
             x = x.view(-1,self.n_features)
         for layer in self.layers_GNN:
             x = F.relu(layer(x, edge_index))
             x = F.dropout(x, p=self.dropout_GNN, training=self.training)
         if self.n_layers_GNN > 0:
-            x = x.view(-1, self.n_features*self.n_hidden_GNN[(self.n_layers_GNN-1)])
-            x = F.relu(self.last_layer_GNN(x))
-            x = F.dropout(x, p=self.dropout_GNN, training=self.training)
+            x = x.view(-1, self.n_features*self.n_hidden_GNN[-1])
         for layer in self.layers_FC:
             x = F.relu(layer(x))
             x = F.dropout(x, p=self.dropout_FC, training=self.training)
@@ -76,8 +80,6 @@ class GraphSAGE(NN):
         if self.n_layers_GNN > 1:
             for i in range(self.n_layers_GNN-1):
                 self.layers_GNN.append(pyg_nn.SAGEConv(n_hidden_GNN[i], n_hidden_GNN[(i+1)]))
-        self.last_layer_GNN = \
-            nn.Linear(n_features*n_hidden_GNN[(self.n_layers_GNN-1)], n_features)
 
 
 class ChebNet(NN):
@@ -97,8 +99,6 @@ class ChebNet(NN):
         if self.n_layers_GNN > 1:
             for i in range(self.n_layers_GNN-1):
                 self.layers_GNN.append(pyg_nn.ChebConv(n_hidden_GNN[i], n_hidden_GNN[(i+1), K]))
-        self.last_layer_GNN = \
-            nn.Linear(n_features*n_hidden_GNN[(self.n_layers_GNN-1)], n_features)
 
 
 class NNConvNet(NN):
@@ -117,5 +117,3 @@ class NNConvNet(NN):
         if self.n_layers_GNN > 1:
             for i in range(self.n_layers_GNN-1):
                 self.layers_GNN.append(pyg_nn.NNConv(n_hidden_GNN[i], n_hidden_GNN[(i+1)]))
-        self.last_layer_GNN = \
-            nn.Linear(n_features*n_hidden_GNN[(self.n_layers_GNN-1)], n_features)
