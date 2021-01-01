0484a964514f626613ac5b9f8f07194b88069330
EliHei2 <55977725+EliHei2@users.noreply.github.com>
2021-01-01 12:28:03 +0100

    Update __init__.py

---
+++ GNNProject/__init__.py
@@ -0,0 +1,11 @@
+__author__ = ', '.join([
+    'Elyas Heidari',
+    'Etienne Sollier',
+    'Aygul Zagidullina'
+])
+
+__email__ = ', '.join([
+    'eheidari@student.ethz.ch',
+    'esollier@student.ethz.ch',
+    'azagidull@student.ethz.ch'
+])
